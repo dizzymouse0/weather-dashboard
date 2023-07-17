31ecd00cd5c3cc593a0b0cd1b5213073be940bec
Dizzymouse0 <npage1221@gmail.com>
2023-07-17 18:42:54 -0500

    adjustments and tweaks

--- assets/script.js
+++ assets/script.js
@@ -10,6 +10,7 @@ var currentWSpeed=$("#wind-speed");
 var currentUvindex= $("#uv-index");
 var sCity=[];
 
+// search local storage if city exists
 function find(c){
     for (var i=0; i<sCity.length; i++){
         if(c.toUpperCase()===sCity[i]){
@@ -19,8 +20,10 @@ function find(c){
     return 1;
 }
 
+// API key variable
 var APIKey="5c8e6d041a648b2382cb982ed4821ece";
 
+// show current weather 
 function displayWeather(event){
     event.preventDefault();
     if(searchCity.val().trim()!==""){
@@ -28,40 +31,40 @@ function displayWeather(event){
         currentWeather(city);
     }
 }
-
+// create AJAX call
 function currentWeather(city){
 
-  var queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIKey;
+    // building url for server data
+    var queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
     $.ajax({
         url:queryURL,
         method:"GET",
     }).then(function(response){
 
-
-      console.log(response);
+    
+        // parse response to display crrent weather with city name, date and weather icon
+        console.log(response);
 
         var weathericon= response.weather[0].icon;
         var iconurl="https://openweathermap.org/img/wn/"+weathericon +"@2x.png";
-
         var date=new Date(response.dt*1000).toLocaleDateString();
-
         $(currentCity).html(response.name +"("+date+")" + "<img src="+iconurl+">");
+        // parse response to show current temperature
 
-
-
+        // converting to fahrenheit
         var tempF = (response.main.temp - 273.15) * 1.80 + 32;
         $(currentTemperature).html((tempF).toFixed(2)+"&#8457");
-
+        // show humidity
         $(currentHumidty).html(response.main.humidity+"%");
     
         var ws=response.wind.speed;
         var windsmph=(ws*2.237).toFixed(1);
         $(currentWSpeed).html(windsmph+"MPH");
    
-
+        //show UV index
         UVIndex(response.coord.lon,response.coord.lat);
         forecast(response.id);
-        if(response.cod==200){
+        if(response.code==200){
             sCity=JSON.parse(localStorage.getItem("cityname"));
             console.log(sCity);
             if (sCity==null){
@@ -82,19 +85,19 @@ function currentWeather(city){
 
     });
 }
-    
+    // returning UV index response
 function UVIndex(ln,lt){
-   
-    var uvqURL="https://api.openweathermap.org/data/2.5/uvi?appid="+ APIKey+"&lat="+lt+"&lon="+ln;
+   // UV index url
+    var uvURL="https://api.openweathermap.org/data/2.5/uvi?appid="+ APIKey+"&lat="+lt+"&lon="+ln;
     $.ajax({
-            url:uvqURL,
+            url:uvURL,
             method:"GET"
             }).then(function(response){
                 $(currentUvindex).html(response.value);
             });
 }
     
-
+// 5-day forecast function
 function forecast(cityid){
     var dayover= false;
     var queryforcastURL="https://api.openweathermap.org/data/2.5/forecast?id="+cityid+"&appid="+APIKey;
@@ -104,6 +107,7 @@ function forecast(cityid){
     }).then(function(response){
         
         for (i=0;i<5;i++){
+            //converting a date to string
             var date= new Date((response.list[((i+1)*8)-1].dt)*1000).toLocaleDateString();
             var iconcode= response.list[((i+1)*8)-1].weather[0].icon;
             var iconurl="https://openweathermap.org/img/wn/"+iconcode+".png";
@@ -120,14 +124,14 @@ function forecast(cityid){
     });
 }
 
-
+// adding passed city on search history
 function addToList(c){
     var listEl= $("<li>"+c.toUpperCase()+"</li>");
     $(listEl).attr("class","list-group-item");
     $(listEl).attr("data-value",c.toUpperCase());
     $(".list-group").append(listEl);
 }
-
+// show past search when list group item is clicked in search history
 function invokePastSearch(event){
     var liEl=event.target;
     if (event.target.matches("li")){
@@ -151,7 +155,7 @@ function loadlastCity(){
     }
 
 }
-//Clear the search history from the page
+// clears search history from the page
 function clearHistory(event){
     event.preventDefault();
     sCity=[];
@@ -159,7 +163,7 @@ function clearHistory(event){
     document.location.reload();
 
 }
-//Click Handlers
+// click Handlers
 $("#search-button").on("click",displayWeather);
 $(document).on("click",invokePastSearch);
 $(window).on("load",loadlastCity);
